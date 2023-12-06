22d32b72ca1406c1fbbf2c08d91a6c6a137c4e98
Brogolem35 <brogolem35@protonmail.com>
2023-12-06 12:54:31 +0300

    Day 6 part1

---
+++ day6/src/bin/part1.rs
@@ -0,0 +1,18 @@
+use regex::Regex;
+
+const PIN: &str = include_str!("../../input.txt");
+
+fn main() {
+    let re = Regex::new(r"\d+").unwrap();
+    let lines: Vec<&str> = PIN.lines().collect();
+    
+    let time = re.find_iter(lines[0]).map(|s| s.as_str().parse::<i64>().unwrap());
+    let dist = re.find_iter(lines[1]).map(|s| s.as_str().parse::<i64>().unwrap());
+    let td = time.zip(dist);
+
+    let time_range = td.map(|(t, d)| (1..=(t-1), d, t));
+
+    let result = time_range.map(|(t, d, tm)| t.filter(move |t| t * (tm - t) > d).count()).reduce(|a, b| a*b).unwrap();
+
+    println!("{result}");
+}
